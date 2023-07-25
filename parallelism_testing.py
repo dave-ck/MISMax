60a110144ee3705793921ccfb37d32839166a627
DCK <devck@protonmail.com>
2023-07-25 15:54:11 +0100

    ncc try2

--- parallelism_testing.py
+++ parallelism_testing.py
@@ -2,17 +2,19 @@
 # import numpy as np
 # from multiprocessing.pool import ThreadPool
 from os import getpid
-from multiprocessing import Pool
+from multiprocessing.pool import ThreadPool
+
+
 # from numba import njit, prange
 # import networkx as nx
 
-
+2
 def hello(i):
     print("PID", getpid())
-    return 2*i
+    return 2 * i
+
 
 if __name__ == "__main__":
-    with Pool(processes=20) as pool:
+    with ThreadPool(processes=20) as pool:
         result = pool.map(hello, [i for i in range(10)])
         print(result)
-
